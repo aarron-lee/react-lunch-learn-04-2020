a7fea6c756bb3425ef0c6dc55add2033d0c2026c
Aarron Lee <aarron.lee@nytimes.com>
2020-04-20 21:20:32 -0400

    working pokemon example that runs dynamically

--- src/slides/renderprops/PokemonExample.jsx
+++ src/slides/renderprops/PokemonExample.jsx
@@ -1,5 +1,17 @@
-import React, { memo, useState, useEffect } from 'react';
-import { Heading, UnorderedList, ListItem } from 'spectacle';
+import React, { memo, useState, useEffect, useCallback } from 'react';
+import debounce from 'lodash/debounce';
+import useForm from '../../hooks/useForm';
+import { Heading, UnorderedList, ListItem, CodePane, indentNormalizer } from 'spectacle';
+import { css } from 'emotion';
+
+const containerStyles = css`
+  display: flex;
+  flex-direction: row;
+`;
+
+const inputStyles = css`
+  font-size: 30px;
+`;
 
 function Query({ query, children }) {
   const [loading, setLoading] = useState(true);
@@ -15,26 +27,55 @@ function Query({ query, children }) {
   return children({ loading, data })
 }
 
+const code = `<Query query="https://pokeapi.co/api/v2/pokemon/\${pokemonName}">
+  {({ loading, data }) => {
+    if (loading) return "Loading..."
+
+    const { name, weight, sprites: { front_default } } = data;
+    return <div>
+      <UnorderedList>
+        <ListItem>Name: {name}</ListItem>
+        <ListItem>Weight: {weight}</ListItem>
+      </UnorderedList>
+      <img src={front_default} />
+    </div>
+  }}
+</Query>`
+
 
 function PokemonExample() {
+  const [pokemonName, setPokemonName] = useState('charmander');
+
+  const { formData, updateField } = useForm({
+    name: 'charmander'
+  });
+
   return (
     <>
       <Heading>Pokemon Example</Heading>
-      <div>
-        <Query query="https://pokeapi.co/api/v2/pokemon/ditto">
-          {({ loading, data }) => {
-            if (loading) return "Loading..."
+      <div className={containerStyles}>
+        <CodePane language="javascript" autoFillHeight>
+          {indentNormalizer(code)}
+        </CodePane>
+        <div>
+          <form onSubmit={e => { e.preventDefault(); e.stopPropagation(); setPokemonName(formData.name) }}>
+            <input className={inputStyles} name="name" value={formData.name || ''} onChange={updateField} />
+          </form>
+          <Query query={`https://pokeapi.co/api/v2/pokemon/${pokemonName}`}>
+            {({ loading, data }) => {
+              if (loading) return "Loading..."
 
-            const { name, weight, sprites: { front_default } } = data;
-            return <div>
-              <UnorderedList>
-                <ListItem>Name: {name}</ListItem>
-                <ListItem>Weight: {weight}</ListItem>
-              </UnorderedList>
-              <img src={front_default} alt={`pokemon ${name}`} style={{ height: '60px' }} />
-            </div>
-          }}
-        </Query>
+              const { name, weight, sprites: { front_default } } = data;
+              return <div>
+                <UnorderedList>
+                  <ListItem>Name: {name}</ListItem>
+                  <ListItem>Weight: {weight}</ListItem>
+                </UnorderedList>
+                <img src={front_default} alt={`pokemon ${name}`} style={{ height: '60px' }} />
+              </div>
+            }}
+          </Query>
+        </div>
       </div>
     </>
   );
